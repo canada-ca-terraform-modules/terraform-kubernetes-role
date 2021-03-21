49197b9e39074c0e2b5b9915e4621fb0f4bbf4e1
Pat Heard <patrick.heard@canada.ca>
2021-03-21 13:17:26 +0000

    feat(examples): add read-only role

--- examples/admin-user-role/main.tf
+++ examples/admin-user-role/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0.0"
+    }
+  }
+  required_version = "~> 0.14.0"
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
 # Create an admin Role in the default namespace
 module "admin-role" {
   source    = "../../"
---
+++ examples/read-only-group-role/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0.0"
+    }
+  }
+  required_version = "~> 0.14.0"
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_namespace" "test" {
+  metadata {
+    name = "test"
+  }
+}
+
+# Create a read-only role in a "test" namespace
+module "read-only-role" {
+  source    = "../../"
+  name      = "read-only-role"
+  namespace = "test"
+  rules = [{
+    api_groups = [""]
+    verbs      = ["get", "list", "watch"]
+    resources  = ["*"]
+    }, {
+    api_groups = ["extensions"]
+    verbs      = ["get", "list", "watch"]
+    resources  = ["*"]
+    }, {
+    api_groups = ["apps"]
+    verbs      = ["get", "list", "watch"]
+    resources  = ["*"]
+    }
+  ]
+  subjects = [{
+    kind = "Group"
+    name = "Readers"
+  }]
+  depends_on = [
+    kubernetes_namespace.test
+  ]
+}
